a6af729b6f8ff324f09a66ce078a1ae33ec388a1
AdityaDJoshi <adirocks1907@gmail.com>
2021-05-11 10:58:47 +0530

    day 7 hackerrank interview prep question

---
+++ hiretest.py
@@ -0,0 +1,26 @@
+#      *
+#     * *
+#    *   *
+#   *  *  *  Author:Aditya Joshi
+#  *       *
+# *         *
+from collections import Counter
+for _ in range(int(input())):
+    l = []
+    n, m = map(int, input().split())
+    x, y = map(int, input().split())
+    for i in range(n):
+        temp = list(input())
+        d = Counter(temp)
+        if 'U' not in d:
+            d['U'] = 0
+        if 'F' not in d:
+            d['F'] = 0
+        if 'P' not in d:
+            d['P'] = 0
+        if d['F'] >= x or ((d['F'] == (x-1)) and (d['P'] >= y)):
+            l.append('1')
+        else:
+            l.append('0')
+
+    print(''.join(l))
