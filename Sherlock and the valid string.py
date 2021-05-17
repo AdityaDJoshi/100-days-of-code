89ebc62148b0457212a78c37f4ca565c6d6ad659
AdityaDJoshi <adirocks1907@gmail.com>
2021-05-18 01:37:32 +0530

    day 12

---
+++ Sherlock and the valid string.py
@@ -0,0 +1,18 @@
+#     _       _ _ _                     _           _     _
+#    / \   __| (_) |_ _   _  __ _      | | ___  ___| |__ (_)
+#   / _ \ / _` | | __| | | |/ _` |  _  | |/ _ \/ __| '_ \| |
+#  / ___ \ (_| | | |_| |_| | (_| | | |_| | (_) \__ \ | | | |
+# /_/   \_\__,_|_|\__|\__, |\__,_|  \___/ \___/|___/_| |_|_|
+#                     |___/
+# https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+from collections import Counter
+s = input()
+d = Counter(Counter(s).values())
+
+if len(d.keys()) == 1:
+    print("YES")
+elif len(d.keys()) == 2 and (1 in d.values()):
+
+    print("YES")
+else:
+    print("NO")
