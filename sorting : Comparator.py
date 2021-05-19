eba7d54169e83da8e89cac9a1725737594edb5a2
AdityaDJoshi <adirocks1907@gmail.com>
2021-05-20 00:01:47 +0530

    day 14

---
+++ sorting : Comparator.py
@@ -0,0 +1,44 @@
+#     _       _ _ _                     _           _     _
+#    / \   __| (_) |_ _   _  __ _      | | ___  ___| |__ (_)
+#   / _ \ / _` | | __| | | |/ _` |  _  | |/ _ \/ __| '_ \| |
+#  / ___ \ (_| | | |_| |_| | (_| | | |_| | (_) \__ \ | | | |
+# /_/   \_\__,_|_|\__|\__, |\__,_|  \___/ \___/|___/_| |_|_|
+#                     |___/
+# https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+from functools import cmp_to_key
+
+
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def __repr__(self):
+        return
+
+    def comparator(a, b):
+        if a.score < b.score:
+            return 1
+        elif a.score == b.score:
+            if a.name > b.name:
+                return 1
+            elif a.name < b.name:
+                return -1
+            else:
+                return 0
+
+        else:
+            return -1
+
+
+n = int(input())
+data = []
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    player = Player(name, score)
+    data.append(player)
+
+data = sorted(data, key=cmp_to_key(Player.comparator))
+for i in data:
+    print(i.name, i.score)
